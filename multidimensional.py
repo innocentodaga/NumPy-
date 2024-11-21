53d1d872419fb011bc94868500a38f1881b36900
tys0n <101554587+innocentodaga@users.noreply.github.com>
2024-11-21 12:37:14 +0300

    numpy

--- multidimensional.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-# Multidimensional array
-a = np.array([[1, 2, 3], [4, 5, 6]], float)
-print(a)
-
-a = np.ndarray.ndim
-print(a)
--- spacyy.py
+++
@@ -1,5 +0,0 @@
-import spacy
-nlp = spacy.load('en')
-doc = nlp(u'I am learning how to build chatbots')
-for token in doc:
-    print(token.text, token.pos)
